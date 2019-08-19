2fa8d0dde1489d7c0e0d0d803c36e397480f88af
carrey <wangkw@i-counting.cn>
2019-08-19 18:20:19 +0800

    [feat] 添加消息总线Bus练习

--- web/src/main/java/com/example/carrey/controller/HelloWordController.java
+++ web/src/main/java/com/example/carrey/controller/HelloWordController.java
@@ -3,6 +3,7 @@ package com.example.carrey.controller;
 import com.example.carrey.config.aspect.WebLog;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
+@RefreshScope
 @RequestMapping("demo-web")
 public class HelloWordController {
 
