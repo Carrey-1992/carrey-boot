f0aeaee6b2e0ca8075f524a580ce4b8dce374711
carrey <wangkw@i-counting.cn>
2019-08-19 12:17:33 +0800

    [feat] 添加简单配置中心练习

--- web/src/main/java/com/example/carrey/controller/HelloWordController.java
+++ web/src/main/java/com/example/carrey/controller/HelloWordController.java
@@ -2,6 +2,7 @@ package com.example.carrey.controller;
 
 import com.example.carrey.config.aspect.WebLog;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +13,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("demo-web")
 public class HelloWordController {
 
+  @Value("${name}")
+  private String name;
+
   @GetMapping("/{name}")
   @WebLog(description = "home")
   public String home(@PathVariable(value = "name") String name) {
     return "Hello " + name;
+  }
+
+
+  @GetMapping("/config")
+  @WebLog(description = "configTest")
+  public String configTest(){
+    return name;
   }
 }
