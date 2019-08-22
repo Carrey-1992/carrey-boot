2c80d8b67d5f6d8fe27b9c340bf354860c95d0e1
carrey <wangkw@i-counting.cn>
2019-08-22 10:23:40 +0800

    [refactor] 将gateway添加到注册中心

--- demo-gateway/src/main/java/com/carrey/gateway/DemoGatewayApplication.java
+++ demo-gateway/src/main/java/com/carrey/gateway/DemoGatewayApplication.java
@@ -2,8 +2,10 @@ package com.carrey.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DemoGatewayApplication {
 
     public static void main(String[] args) {
