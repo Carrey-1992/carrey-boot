c6be36dc1a12181a2c390d4d9d718e1e2c015ec7
carrey <15510879425@163.com>
2019-09-02 23:04:05 +0800

    [feat] add mybatis CURD demo test

--- demo-mybatis-plus/mybatis-plus-quickstart/src/main/java/com/carrey/quickstart/mapper/UserMapper.java
+++ demo-mybatis-plus/mybatis-plus-quickstart/src/main/java/com/carrey/quickstart/mapper/UserMapper.java
@@ -4,13 +4,15 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.carrey.quickstart.entity.User;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
+
 public interface UserMapper extends BaseMapper<User> {
   /**
    * 根据ID获取对象.
-   * @param id
+   * @param user
    * @return
    */
-  User getInfo(@Param("id") Integer id);
+  User getInfo(User user);
 
   /**
    * 创建User.
@@ -18,4 +20,32 @@ public interface UserMapper extends BaseMapper<User> {
    * @return
    */
   Integer createUser(User user);
+
+  /**
+   * 通过主键更新.
+   * @return
+   */
+  int updateById(User user);
+
+
+  /**
+   * 通过主键删除信息.
+   * @param id
+   * @return
+   */
+  int deleteById(long id);
+
+  /**
+   * 通过主键集合获取对象集合.
+   * @param ids
+   * @return
+   */
+  List<User> selectByIds(@Param("ids") List<Long> ids);
+
+  /**
+   * 批量插入.
+   * @param userList
+   * @return
+   */
+  int insertList(List<User> userList);
 }
--- demo-mybatis-plus/mybatis-plus-quickstart/src/test/java/com/carrey/quickstart/SampleTest.groovy
+++ demo-mybatis-plus/mybatis-plus-quickstart/src/test/java/com/carrey/quickstart/SampleTest.groovy
@@ -2,12 +2,16 @@ package com.carrey.quickstart
 
 import com.carrey.quickstart.entity.User
 import com.carrey.quickstart.mapper.UserMapper
+import org.assertj.core.util.Lists
 import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.annotation.Rollback
 import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.transaction.annotation.Transactional
+import sun.plugin2.jvm.RemoteJVMLauncher
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -17,14 +21,22 @@ class SampleTest {
     private UserMapper userMapper
 
     @Test
+    @Transactional
+    @Rollback
     void testSelect() {
         println "----- selectAll method test ------"
-        def user = userMapper.getInfo  1
-        Assert.assertEquals "Jone", user.getName()
+        User user = new User()
+        user.setId 5
+        user.setName 'Billie'
+        user.setAge 24
+        user = userMapper.getInfo  user
+        Assert.assertEquals "Billie", user.getName()
         println user
     }
 
     @Test
+    @Transactional
+    @Rollback
     void testInsert() {
         println "----- insertAll method test ------"
         User user = new User()
@@ -32,8 +44,61 @@ class SampleTest {
         user.setName 'Carrey'
         user.setAge 24
         user.setEmail 'test6@baomidou.com'
-        def idex = userMapper.createUser(user)
-        Assert.assertEquals 1,idex
-        println idex
+        def id = userMapper.createUser(user)
+        Assert.assertEquals 1,id
+        println id
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    void testUpdateById() {
+        println "----- updateAll method test ------"
+        User user = new User()
+        user.setId 5
+        user.setName 'Carrey'
+        user.setAge 24
+        user.setEmail 'test6@baomidou.com'
+        def index = userMapper.updateById(user)
+        Assert.assertEquals 1,index
+        println index
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    void testDeleteById() {
+        println "----- updateAll method test ------"
+        def index = userMapper.deleteById(1L)
+        Assert.assertEquals 1,index
+        println index
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    void testSelectByIds() {
+        println "----- selectByIds method test ------"
+        def userList = userMapper.selectByIds(Lists.newArrayList(1L,2L))
+        Assert.assertEquals 2,userList.size()
+        userList.each {print it}
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    void testInsetList() {
+        def userList = []
+        5.times {
+            User user = new User()
+            user.setId it + 6L
+            user.setName 'Carrey'+ (it + 6)
+            user.setAge 24
+            user.setEmail 'test'+(it + 6)+'@baomidou.com'
+            userList.add(user)
+        }
+        def number = userMapper.insertList(userList)
+        Assert.assertEquals(5,number)
+        println number
     }
 }
