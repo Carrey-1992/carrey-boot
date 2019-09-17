cc877e6b90744d6253fd0eba8b7e284f2b8601a2
carrey <15510879425@163.com>
2019-09-17 13:50:09 +0800

    [feat] add MyExecutorPlugin demo

---
+++ common/src/main/java/com/example/carrey/mybatis/BaseEntity.java
@@ -0,0 +1,30 @@
+package com.example.carrey.mybatis;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class BaseEntity {
+
+  /**
+   * 创建人主键.
+   */
+  @CreateBy
+  private Long createBy;
+  /**
+   * 更新人主键.
+   */
+  @UpdateBy
+  private Long updateBy;
+  /**
+   * 创建时间.
+   */
+  @CreateAt
+  private Date createAt;
+  /**
+   * 更新时间.
+   */
+  @UpdateAt
+  private Date updateAt;
+}
--- demo-mybatis-plus/mybatis-plus-quickstart/src/main/java/com/carrey/quickstart/config/MyBatisPluginConfig.java
+++ demo-mybatis-plus/mybatis-plus-quickstart/src/main/java/com/carrey/quickstart/config/MyBatisPluginConfig.java
@@ -1,22 +1,22 @@
 package com.carrey.quickstart.config;
 
-import com.carrey.quickstart.MyExecutorPlugin;
+import com.example.carrey.mybatis.MyExecutorPlugin;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Properties;
+
 @Configuration
-@MapperScan({"com.carrey.quickstart.mapper"})
-public class MapperConfig {
-  //注册插件
+public class MyBatisPluginConfig {
   @Bean
-  public MyExecutorPlugin myPlugin() {
+  public MyExecutorPlugin myExecutorPlugin(){
     MyExecutorPlugin myPlugin = new MyExecutorPlugin();
     //设置参数，比如阈值等，可以在配置文件中配置，这里直接写死便于测试
-    //Properties properties = new Properties();
+    Properties properties = new Properties();
     //这里设置慢查询阈值为1毫秒，便于测试
-    //properties.setProperty("time", "1");
-    //myPlugin.setProperties(properties);
+    properties.setProperty("test", "Carrey");
+    myPlugin.setProperties(properties);
     return myPlugin;
   }
 }
