b8677d45a1727f6aefd07b11a3daa3eea1c68def
Carrey <15510879425@163.com>
2020-03-01 11:21:05 +0800

    [feat] DynamicProxyTest

---
+++ demo-spring-cloud/web/src/test/java/com/example/carrey/设计模式/代理模式/动态代理/SpendthriftProxyHandler.java
@@ -0,0 +1,33 @@
+package com.example.carrey.设计模式.代理模式.动态代理;
+
+import org.springframework.util.ReflectionUtils;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.util.Objects;
+
+public class SpendthriftProxyHandler<T> implements InvocationHandler {
+
+    private T proxiedObject;
+
+    public SpendthriftProxyHandler(T proxiedObject) {
+        this.proxiedObject = proxiedObject;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        if (Objects.equals(method.getName(),"eat")) {
+            System.out.println("找小明吃饭前得先准备好酒！有菜没酒不够朋友！");
+        }
+        if (Objects.equals(method.getName(),"drink")) {
+            System.out.println("找小明喝酒前得不用准备好菜！有酒没菜不算慢待！");
+        }
+
+        Object result = ReflectionUtils.invokeMethod(method, proxiedObject, args);
+
+        if (Objects.equals(method.getName(),"play")) {
+            System.out.println("哪里有妹子去哪里玩！");
+        }
+        return result;
+    }
+}
