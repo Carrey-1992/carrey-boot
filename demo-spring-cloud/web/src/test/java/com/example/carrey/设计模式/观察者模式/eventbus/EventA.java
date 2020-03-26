7b4ce08c3656cf3f8a1c095856b288f00842d793
Conway <15510879425@163.com>
2020-03-26 18:22:29 +0800

    [feat] add EventBusTest

---
+++ demo-spring-cloud/web/src/test/java/com/example/carrey/设计模式/观察者模式/eventbus/EventA.java
@@ -0,0 +1,10 @@
+package com.example.carrey.设计模式.观察者模式.eventbus;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class EventA {
+    private String message;
+}
---
+++ demo-spring-cloud/web/src/test/java/com/example/carrey/设计模式/观察者模式/eventbus/EventB.java
@@ -0,0 +1,8 @@
+package com.example.carrey.设计模式.观察者模式.eventbus;
+
+public class EventB extends EventA{
+
+    public EventB(String message) {
+        super(message);
+    }
+}
