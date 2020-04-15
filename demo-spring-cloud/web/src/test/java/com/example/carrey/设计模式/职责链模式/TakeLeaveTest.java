752c9b89dd5d57ff278a359ecd6f1bd17e9e9ecd
carrey <15510879425@163.com>
2020-04-15 10:50:07 +0800

    [test] add TakeLeaveHandlerChain2

--- demo-spring-cloud/web/src/test/java/com/example/carrey/设计模式/职责链模式/TakeLeaveTest.java
+++ demo-spring-cloud/web/src/test/java/com/example/carrey/设计模式/职责链模式/TakeLeaveTest.java
@@ -22,4 +22,23 @@ public class TakeLeaveTest {
         }
         System.out.println("小明最多能请"+ (day - 1) +"天假！");
     }
+
+    @Test
+    public void test2() {
+        System.out.println("小明想试试最多能请几天假");
+        boolean flag = true;
+        double day = 1.0;
+        while (flag) {
+            flag = new TakeLeaveHandlerChain2()
+                    .setHandler(new SupervisorTakeLeaveHandler())
+                    .setHandler(new ManagerTakeLeaveHandler())
+                    .setHandler(new GeneralManagerTakeLeaveHandler())
+                    .doHandle(day);
+            if (flag) {
+                day++;
+            }
+            System.out.println("-----------------------------------------------------------");
+        }
+        System.out.println("小明最多能请"+ (day - 1) +"天假！");
+    }
 }
