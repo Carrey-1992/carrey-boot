bb76c78d3fc735af0f6da259a67d743b68d34517
Conway <15510879425@163.com>
2020-05-07 16:02:55 +0800

    [feat] add demo-Hutool

---
+++ demo-hutool/src/test/java/com/carrey/demohutool/DemoHutoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demohutool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoHutoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
