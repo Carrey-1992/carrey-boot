136d429ef6cffb73834bef23f7d3cb79d5b02f29
Conway <15510879425@163.com>
2021-01-21 17:23:59 +0800

    [feat] 算法

---
+++ demo-redis/src/main/java/com/example/carrey/WebApplication.java
@@ -0,0 +1,17 @@
+package com.example.carrey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Conway
+ * @className Application
+ * @description
+ * @date 2021/1/18 下午2:28
+ */
+@SpringBootApplication
+public class WebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class,args);
+    }
+}
