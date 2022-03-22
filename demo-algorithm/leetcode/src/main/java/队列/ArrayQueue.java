963300e7ee823f6d8b48e99a2dcd78c8f74efc4c
Conway <15510879425@163.com>
2022-03-22 21:11:59 +0800

    [feat] ArrayQueue

--- demo-algorithm/leetcode/src/main/java/队列/ArrayQueue.java
+++ demo-algorithm/leetcode/src/main/java/队列/ArrayQueue.java
@@ -9,16 +9,20 @@ package 队列;
 public class ArrayQueue<T> {
 
     public static void main(String[] args) {
-        ArrayQueue<Integer> arrayQueue = new ArrayQueue<>(10000);
+        ArrayQueue<Integer> arrayQueue = new ArrayQueue<>(100);
 
-        for (int i = 0 ; i < 10000 ; i++) {
+        for (int i = 0; i < 100; i++) {
             arrayQueue.add(i + 1);
         }
 
-        for (int i = 0 ; i < 10000 ; i++) {
+        for (int i = 0; i < 50; i++) {
             int size = arrayQueue.size();
-            Integer res = arrayQueue.remove();
-            System.out.println("当前队列大小为："+size+"输出结果："+ res);
+            Integer res = arrayQueue.poll();
+            System.out.println("当前队列大小为：" + size + "输出结果：" + res);
+        }
+
+        for (int i = 50; i < 100; i++) {
+            arrayQueue.add(i + 1);
         }
     }
 
@@ -35,7 +39,7 @@ public class ArrayQueue<T> {
     }
 
     public ArrayQueue(int capacity) {
-        this.queue =  (T[]) new Object[capacity+1];
+        this.queue = (T[]) new Object[capacity];
         this.head = 0;
         this.tail = 0;
         this.size = 0;
@@ -43,6 +47,7 @@ public class ArrayQueue<T> {
 
     /**
      * 添加元素
+     *
      * @param t
      * @return
      */
@@ -50,81 +55,39 @@ public class ArrayQueue<T> {
         if (null == t) {
             throw new NullPointerException();
         }
-        if (queue.length == tail + 1) {
+        if (queue.length == size) {
             throw new IndexOutOfBoundsException("Queue full");
         }
-        tail++;
         queue[tail] = t;
-        size ++;
+        tail++;
+        size++;
         return true;
     }
 
-    /**
-     * 将指定的元素插入此数据块的末尾
-     * @param t
-     * @return
-     */
-    public boolean offer(T t) {
-        if (null == t) {
-            throw new NullPointerException();
-        }
-        if (queue.length == tail + 1) {
-            return false;
-        }
-        tail++;
-        queue[tail] = t;
-        return true;
-    }
 
     /**
-     * 删除队尾元素（如果队尾元素为空则抛出异常）
-     * @return
-     */
-    public T remove() {
-        if (size == 0) {
-            throw new IndexOutOfBoundsException("Queue empty");
-        }
-        T res = queue[tail];
-        queue[tail] = null;
-        tail--;
-        size --;
-        return res;
-    }
-
-    /**
-     * 获取队尾元素，并删除队尾元素（如果队尾元素为空则返回null）
+     * 获取队头元素，并删除队尾元素（如果队尾元素为空则返回null）
+     *
      * @return
      */
     public T poll() {
         if (size == 0) {
             return null;
         }
-        T res = queue[tail];
-        queue[tail] = null;
-        tail--;
-        size --;
+        T res = queue[head];
+        queue[head] = null;
+        head++;
+        size--;
+        //如果head的index已经大于queue的size的一半则要迁移数据
+        if (head > (queue.length - 1) / 2) {
+            for (int i = queue.length - 1; i >= queue.length - head; i--) {
+                queue[i - head] = queue[i];
+                queue[i] = null;
+            }
+            tail = queue.length - head - 1;
+            head = 0;
+        }
         return res;
     }
 
-    /**
-     * 获取队尾元素，如果队尾元素不存在则抛出异常
-     * @return
-     */
-    public T element() {
-        if (size == 0) {
-            throw new IndexOutOfBoundsException("Queue empty");
-        }
-        return queue[tail];
-    }
-
-    /**
-     * 获取队尾元素，如果队尾元素不存在则返回null
-     * @return
-     */
-    public T peek() {
-        if (size == 0) {
-            return null;
-        }
-        return queue[tail];
-    }
 }
